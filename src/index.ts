be53fe2c95d4ecda9fd00e5971f681620c4c38e8
leemhoon00 <leemhoon000@gmail.com>
2024-03-08 22:50:49 +0900

    feat: get labels by json format

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
 import { Octokit } from '@octokit/rest';
+import { Label } from './interface/label';
 
-export async function getLabels(authToken?: string) {
-  authToken;
-  const octokit = new Octokit();
+export class Copier {
+  private octokit: Octokit;
+  constructor(githubToken?: string) {
+    if (!githubToken) {
+      this.octokit = new Octokit();
+    } else {
+      this.octokit = new Octokit({
+        auth: githubToken,
+      });
+    }
+  }
 
-  const labels = await octokit.issues.listLabelsForRepo({
-    owner: 'leemhoon00',
-    repo: 'typevocab-server',
-  });
+  async getLabels(owner: string, repo: string) {
+    const result = await this.octokit.request(
+      `GET /repos/${owner}/${repo}/labels`,
+      {
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      }
+    );
 
-  return labels;
+    const data = result.data as Label[];
+    return data.map((label) => {
+      return {
+        name: label.name,
+        color: label.color,
+        description: label.description,
+      };
+    });
+  }
 }
