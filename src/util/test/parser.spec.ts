ed6040b24eb4e188a4251837a305d45d9d3a2f94
leemhoon00 <leemhoon000@gmail.com>
2024-03-14 22:20:36 +0900

    test: Parser

---
+++ src/util/test/parser.spec.ts
@@ -0,0 +1,74 @@
+import { Parser } from '../parser';
+import { GithubLabel, Label } from '../../domain/label';
+
+describe('Parser', () => {
+  describe('parseGithubLabel', () => {
+    it('should return a parsed label', () => {
+      // given
+      const githubLabel: GithubLabel = {
+        name: 'label',
+        color: 'ffffff',
+        description: 'description',
+        id: 1,
+        node_id: 'node_id',
+        url: 'url',
+        default: false,
+      };
+
+      // when
+      const parser = new Parser();
+      const label: Label = parser.parseGithubLabel(githubLabel);
+
+      // then
+      expect(label).toEqual({
+        name: 'label',
+        color: 'ffffff',
+        description: 'description',
+      });
+    });
+  });
+
+  describe('parseGithubLabels', () => {
+    it('should return a parsed labels', () => {
+      // given
+      const githubLabels: GithubLabel[] = [
+        {
+          name: 'label1',
+          color: 'ffffff',
+          description: 'description1',
+          id: 1,
+          node_id: 'node_id1',
+          url: 'url1',
+          default: false,
+        },
+        {
+          name: 'label2',
+          color: '000000',
+          description: 'description2',
+          id: 2,
+          node_id: 'node_id2',
+          url: 'url2',
+          default: false,
+        },
+      ];
+
+      // when
+      const parser = new Parser();
+      const labels: Label[] = parser.parseGithubLabels(githubLabels);
+
+      // then
+      expect(labels).toEqual([
+        {
+          name: 'label1',
+          color: 'ffffff',
+          description: 'description1',
+        },
+        {
+          name: 'label2',
+          color: '000000',
+          description: 'description2',
+        },
+      ]);
+    });
+  });
+});
