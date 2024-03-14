f7024831b1b6345d834a3c05bbfdb21e9d434975
leemhoon00 <leemhoon000@gmail.com>
2024-03-14 22:03:17 +0900

    test: octokitService - deleteLabel

--- src/service/test/octokit.spec.ts
+++ src/service/test/octokit.spec.ts
@@ -23,8 +23,8 @@ describe('OctokitService', () => {
     it('should call the Octokit API with the correct parameters', async () => {
       const spy = jest.spyOn(octokitMock, 'request').mockResolvedValue({
         data: [
-          { name: 'label1', color: 'ff0000' },
-          { name: 'label2', color: '00ff00' },
+          { name: 'label1', color: 'ff0000', description: '' },
+          { name: 'label2', color: '00ff00', description: '' },
         ],
       } as any);
 
@@ -38,6 +38,64 @@ describe('OctokitService', () => {
       // then
       expect(spy).toHaveBeenCalledWith(
         `GET /repos/${owner}/${repo}/labels`,
+        octokitOption
+      );
+    });
+  });
+
+  describe('deleteLabel', () => {
+    it('should call the Octokit API with the correct parameters', async () => {
+      const spy = jest
+        .spyOn(octokitMock, 'request')
+        .mockResolvedValue({} as any);
+
+      // given
+      const owner = 'owner';
+      const repo = 'repo';
+      const label = { name: 'label1', color: 'ff0000', description: '' };
+
+      // when
+      await octokitService.deleteLabel({ owner, repo, label });
+
+      // then
+      expect(spy).toHaveBeenCalledWith(
+        `DELETE /repos/${owner}/${repo}/labels/label1`,
+        octokitOption
+      );
+    });
+
+    it('should call the Octokit API with the correct parameters when the label name contains spaces', () => {
+      // given
+      const owner = 'owner';
+      const repo = 'repo';
+      const label = { name: 'space label', color: 'ff0000', description: '' };
+
+      // when
+      octokitService.deleteLabel({ owner, repo, label });
+
+      // then
+      const transformedLabelName = 'space%20label';
+
+      expect(octokitMock.request).toHaveBeenCalledWith(
+        `DELETE /repos/${owner}/${repo}/labels/${transformedLabelName}`,
+        octokitOption
+      );
+    });
+
+    it('should call the Octokit API with the correct parameters when the label name contains colons', () => {
+      // given
+      const owner = 'owner';
+      const repo = 'repo';
+      const label = { name: 'colon:label:', color: 'ff0000', description: '' };
+
+      // when
+      octokitService.deleteLabel({ owner, repo, label });
+
+      // then
+      const transformedLabelName = 'colon%3Alabel%3A';
+
+      expect(octokitMock.request).toHaveBeenCalledWith(
+        `DELETE /repos/${owner}/${repo}/labels/${transformedLabelName}`,
         octokitOption
       );
     });
