9018464431ba4a7b79d6dd406889760b3900a9f2
leemhoon00 <leemhoon000@gmail.com>
2024-03-14 20:55:40 +0900

    test: label.spec.ts (isEqualLabel)

---
+++ src/domain/test/label.spec.ts
@@ -0,0 +1,85 @@
+import { isEqualLabel, Label } from '../label';
+
+describe('label', () => {
+  describe('isEqualLabel', () => {
+    it('should return false when names are different', () => {
+      // given
+      const label1: Label = {
+        name: 'bug',
+        color: '000000',
+        description: 'its bug',
+      };
+      const label2: Label = {
+        name: 'bug2',
+        color: '000000',
+        description: 'its bug',
+      };
+
+      // when
+      const result = isEqualLabel(label1, label2);
+
+      // then
+      expect(result).toBe(false);
+    });
+
+    it('should return false when colors are different', () => {
+      // given
+      const label1: Label = {
+        name: 'bug',
+        color: '000000',
+        description: 'its bug',
+      };
+      const label2: Label = {
+        name: 'bug',
+        color: 'ffffff',
+        description: 'its bug',
+      };
+
+      // when
+      const result = isEqualLabel(label1, label2);
+
+      // then
+      expect(result).toBe(false);
+    });
+
+    it('should return false when descriptions are different', () => {
+      // given
+      const label1: Label = {
+        name: 'bug',
+        color: '000000',
+        description: 'its bug',
+      };
+      const label2: Label = {
+        name: 'bug',
+        color: '000000',
+        description: 'its bug2',
+      };
+
+      // when
+      const result = isEqualLabel(label1, label2);
+
+      // then
+      expect(result).toBe(false);
+    });
+
+    it('should return true when labels are equal', () => {
+      // given
+      const label1: Label = {
+        name: 'bug',
+        color: '000000',
+        description: 'its bug',
+      };
+      const label2: Label = {
+        name: 'bug',
+        color: '000000',
+        description: 'its bug',
+      };
+
+      // when
+      const result = isEqualLabel(label1, label2);
+
+      // then
+      expect(result).toBe(true);
+    });
+  });
+});
--- src/test/factory.spec.ts
+++ src/test/factory.spec.ts
@@ -1,15 +1,17 @@
 import { createCopier } from '../factory';
 import { DefaultCopier, TokenCopier } from '../controller';
 
-describe('createCopier', () => {
-  it('should return an instance of DefaultCopier when no token is provided', () => {
-    const copier = createCopier();
-    expect(copier).toBeInstanceOf(DefaultCopier);
-  });
+describe('factory', () => {
+  describe('createCopier', () => {
+    it('should return an instance of DefaultCopier when no token is provided', () => {
+      const copier = createCopier();
+      expect(copier).toBeInstanceOf(DefaultCopier);
+    });
 
-  it('should return an instance of TokenCopier when a token is provided', () => {
-    const token = 'your-github-token';
-    const copier = createCopier(token);
-    expect(copier).toBeInstanceOf(TokenCopier);
+    it('should return an instance of TokenCopier when a token is provided', () => {
+      const token = 'your-github-token';
+      const copier = createCopier(token);
+      expect(copier).toBeInstanceOf(TokenCopier);
+    });
   });
 });
