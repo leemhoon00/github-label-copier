f1c3135d6a96ce5ea98370ac3659a70ee735aa2a
leemhoon00 <leemhoon000@gmail.com>
2024-03-15 23:06:25 +0900

    test: TokenCopier - copyLabels method

--- test/util/controller.spec.ts
+++ test/util/controller.spec.ts
@@ -9,11 +9,8 @@ describe('DefaultCopier', () => {
   let tokenCopier: TokenCopier;
 
   beforeEach(() => {
-    // Create a mock OctokitService
     const octokitServiceMock = {
       getLabels: jest.fn(),
-      deleteLabel: jest.fn(),
-      createLabel: jest.fn(),
     } as unknown as OctokitService;
 
     tokenCopier = new TokenCopier(octokitServiceMock);
@@ -68,6 +65,122 @@ describe('DefaultCopier', () => {
           { name: 'name1', color: '000000', description: 'description1' },
           { name: 'name2', color: '000000', description: 'description2' },
         ])
+      );
+    });
+  });
+});
+
+describe('TokenCopier', () => {
+  let tokenCopier: TokenCopier;
+  let octokitServiceMock: jest.Mocked<OctokitService>;
+
+  beforeEach(() => {
+    octokitServiceMock = {
+      getLabels: jest.fn(),
+      deleteLabel: jest.fn(),
+      createLabel: jest.fn(),
+    } as unknown as jest.Mocked<OctokitService>;
+
+    tokenCopier = new TokenCopier(octokitServiceMock);
+  });
+
+  describe('copyLabels', () => {
+    it('should delete one label and create two labels', async () => {
+      //given
+      octokitServiceMock.getLabels
+        .mockResolvedValueOnce([
+          { name: 'name2', color: '000000', description: 'description2' },
+          { name: 'name3', color: '000000', description: 'description3' },
+          { name: 'name4', color: '000000', description: 'description4' },
+        ])
+        .mockResolvedValueOnce([
+          { name: 'name1', color: '000000', description: 'description1' },
+          { name: 'name2', color: '000000', description: 'description2' },
+        ]);
+
+      // when
+      await tokenCopier.copyLabels({
+        from: 'https://github.com/leemhoon00/github-label-copier',
+        to: 'https://github.com/leemhoon00/github-label-copier2',
+      });
+
+      // then
+      expect(octokitServiceMock.deleteLabel).toHaveBeenCalledTimes(1);
+      expect(octokitServiceMock.deleteLabel).toHaveBeenCalledWith({
+        owner: 'leemhoon00',
+        repo: 'github-label-copier2',
+        label: { name: 'name1', color: '000000', description: 'description1' },
+      });
+      expect(octokitServiceMock.createLabel).toHaveBeenCalledTimes(2);
+      expect(octokitServiceMock.createLabel).toHaveBeenCalledWith(
+        {
+          owner: 'leemhoon00',
+          repo: 'github-label-copier2',
+        },
+        { name: 'name3', color: '000000', description: 'description3' }
+      );
+      expect(octokitServiceMock.createLabel).toHaveBeenCalledWith(
+        {
+          owner: 'leemhoon00',
+          repo: 'github-label-copier2',
+        },
+        { name: 'name4', color: '000000', description: 'description4' }
+      );
+    });
+
+    it('should delete label when color or description is different', async () => {
+      //given
+      octokitServiceMock.getLabels
+        .mockResolvedValueOnce([
+          { name: 'name1', color: '000000', description: 'description1' },
+          { name: 'name2', color: '000000', description: 'description2' },
+        ])
+        .mockResolvedValueOnce([
+          { name: 'name1', color: 'ffffff', description: 'description1' },
+          {
+            name: 'name2',
+            color: '000000',
+            description: 'different description',
+          },
+        ]);
+
+      // when
+      await tokenCopier.copyLabels({
+        from: 'https://github.com/leemhoon00/github-label-copier',
+        to: 'https://github.com/leemhoon00/github-label-copier2',
+      });
+
+      // then
+      expect(octokitServiceMock.deleteLabel).toHaveBeenCalledTimes(2);
+      expect(octokitServiceMock.deleteLabel).toHaveBeenCalledWith({
+        owner: 'leemhoon00',
+        repo: 'github-label-copier2',
+        label: { name: 'name1', color: 'ffffff', description: 'description1' },
+      });
+      expect(octokitServiceMock.deleteLabel).toHaveBeenCalledWith({
+        owner: 'leemhoon00',
+        repo: 'github-label-copier2',
+        label: {
+          name: 'name2',
+          color: '000000',
+          description: 'different description',
+        },
+      });
+
+      expect(octokitServiceMock.createLabel).toHaveBeenCalledTimes(2);
+      expect(octokitServiceMock.createLabel).toHaveBeenCalledWith(
+        {
+          owner: 'leemhoon00',
+          repo: 'github-label-copier2',
+        },
+        { name: 'name1', color: '000000', description: 'description1' }
+      );
+      expect(octokitServiceMock.createLabel).toHaveBeenCalledWith(
+        {
+          owner: 'leemhoon00',
+          repo: 'github-label-copier2',
+        },
+        { name: 'name2', color: '000000', description: 'description2' }
       );
     });
   });
